aa7ffb73aa8fca3e26f80b4b845f7f54d075bf95
jennifervan <jennifern09@gmail.com>
2015-09-16 18:40:38 -0700

    Fix indentations

--- hw/hw04/break_me.py
+++ hw/hw04/break_me.py
@@ -1,9 +1,9 @@
 def exhibit_name_error():
-	exhibitNameError
+    exhibitNameError
 
 def exhibit_attribute_error():
-	myInt = 3
-	myInt.append(5)
+    myInt = 3
+    myInt.append(5)
 
 def exhibit_type_error():
-	sum = "Hello" + 1 
+    sum = "Hello" + 1
