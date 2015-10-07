6290b0aef38daa45a7cb674267303f0da5c71b8a
jennifervan <jennifern09@gmail.com>
2015-10-07 00:46:22 -0700

    Add comments

--- hw/hw13/trigrams.py
+++ hw/hw13/trigrams.py
@@ -7,12 +7,14 @@ file.close()
 words = []
 trigrams = {}
 
+# Remove punctuation and store words
 for line in lines:
     line = line.replace('--', ' ').replace('\n', '').replace('(', '')
     line = line.replace(')', '').replace('-', ' ').replace(',', '')
     line = line.replace('"', '').replace("'", '')
     words += line.split(' ')
 
+# Build trigrams dictionary
 for i in range(len(words) - 2):
     key = words[i] + ' ' + words[i + 1]
     if key in trigrams:
@@ -20,15 +22,18 @@ for i in range(len(words) - 2):
     else:
         trigrams[key] = [words[i + 2]]
 
+# Initialize trigram
 start = random.choice(list(trigrams.keys()))
 trigram = start + ' ' + random.choice(trigrams[start])
 
+# Build book, starting with initialize trigram
 book = "" + trigram
 trigram = trigram.split()
 nextkey = trigram[1] + ' ' + trigram[2]
 
 count = 0
 
+# Continue book to 225 word and while key in trigram dictionary
 while (count < 225) and (nextkey in trigrams.keys()):
     nextword = random.choice(trigrams[nextkey])
     book = book + ' ' + nextword
@@ -39,7 +44,10 @@ while (count < 225) and (nextkey in trigrams.keys()):
         nextkey = random.choice(list(trigrams.keys()))
     count = count + 1
 
+# Capitalize first word and end with period
+book = book.capitalize()
 if book[-1:] != '.':
     book = book + '.'
 
+# Print book
 print(book)
